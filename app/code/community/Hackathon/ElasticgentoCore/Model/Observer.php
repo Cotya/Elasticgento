7a46a3a6c6376ea9b52da2e1e11ff61714dace7b
Flyingmana <flyingmana@googlemail.com>
2015-01-07 01:13:56 +0100

    add optional cronjob to do complete reindex of elastic search
    default: off

---
+++ app/code/community/Hackathon/ElasticgentoCore/Model/Observer.php
@@ -0,0 +1,15 @@
+<?php
+
+class Hackathon_ElasticgentoCore_Model_Observer
+{
+    
+    public function fullReindex()
+    {
+        if(!Mage::getStoreConfig('elasticgento/cron/is_active')){
+            return;
+        }
+        $indexer = new Hackathon_ElasticgentoCore_Model_Resource_Catalog_Product_Indexer_Elasticgento();
+        $indexer->reindexAll();
+    }
+    
+}
