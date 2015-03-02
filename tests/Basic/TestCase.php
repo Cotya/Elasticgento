87fb2e31c491bde7e149a7b9d1d66681def0b8a5
Flyingmana <flyingmana@googlemail.com>
2015-03-02 02:21:44 +0100

    basic phpunit skeleton

---
+++ tests/Basic/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Cotya\Elasticgento\Test\Basic;
+
+class CaseTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testWorld()
+    {
+        $this->assertTrue(true);
+    }
+}
