99cff673ace7bfdf1a1db800d88d1ded4efe7116
Flyingmana <flyingmana@googlemail.com>
2015-03-02 03:06:51 +0100

    add some more output to setup script

--- tests/setup_travis.php
+++ tests/setup_travis.php
@@ -9,9 +9,15 @@
 
 $magentoTargetVersion = getenv('MAGENTO');
 
+$passthru = function($shellCommand) {
+    echo $shellCommand.PHP_EOL;
+    passthru($shellCommand);
+};
+
 if (strpos($magentoTargetVersion, '1.')===0) {
-    passthru('vendor/bin/mage-ci install test-root $MAGENTO magento_test -c -t -r http://mage-ci.ecomdev.org');
-    passthru('vendor/bin/mage-ci install-module test-root $CURR_DIR');
+    $passthru('vendor/bin/mage-ci install test-root $MAGENTO magento_test -c -t -r http://mage-ci.ecomdev.org');
+    $passthru('echo $CURR_DIR');
+    $passthru('vendor/bin/mage-ci install-module test-root $CURR_DIR');
 } elseif (strpos($magentoTargetVersion, '2.')===0) {
     echo "I think we need a way to install mage2 in a similar way".PHP_EOL;
 } else {
