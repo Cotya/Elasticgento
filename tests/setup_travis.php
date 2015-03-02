1112d9f34e2a5d0f61dd5038d83a1123e7f071c6
Flyingmana <flyingmana@googlemail.com>
2015-03-02 03:04:27 +0100

    add some End of Lines to setup script

--- tests/setup_travis.php
+++ tests/setup_travis.php
@@ -13,8 +13,8 @@ if (strpos($magentoTargetVersion, '1.')===0) {
     passthru('vendor/bin/mage-ci install test-root $MAGENTO magento_test -c -t -r http://mage-ci.ecomdev.org');
     passthru('vendor/bin/mage-ci install-module test-root $CURR_DIR');
 } elseif (strpos($magentoTargetVersion, '2.')===0) {
-    echo "I think we need a way to install mage2 in a similar way";
+    echo "I think we need a way to install mage2 in a similar way".PHP_EOL;
 } else {
-    fwrite(STDERR, "Version Identifier '$magentoTargetVersion' not recogniezed");
+    fwrite(STDERR, "Version Identifier '$magentoTargetVersion' not recogniezed".PHP_EOL);
     die(1);
 };
